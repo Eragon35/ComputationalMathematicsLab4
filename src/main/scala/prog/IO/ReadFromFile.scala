38a45289903b37528b863853d7b448467ba3917f
Eragon35 <antipin35@yahoo.com>
2021-04-23 20:28:57 +0300

    Rework reading from file

--- src/main/scala/prog/IO/ReadFromFile.scala
+++ src/main/scala/prog/IO/ReadFromFile.scala
@@ -9,13 +9,16 @@ object ReadFromFile {
     try {
       FileChecker.check(fileName)
       val source = Source.fromFile(fileName)
-      val line = source.getLines().next().split(" ").map(x => x.toDouble)
-      array = array :+ (line(0), line(1))
+      for (line <- source.getLines()) {
+        var value = line.replace(',', '.').split(" ").map(x => x.toDouble)
+        array = array :+ (value(0), value(1))
+      }
       source.close()
     } catch {
       case e: Throwable => Console.err.println("\tProblem with parsing file\n" + e.getMessage)
         e.printStackTrace()
     }
+    println(s"Из файла прочитано ${array.length} точек")
     if (array.length < 12) throw new IllegalArgumentException("Кол-во точек должно быть больше либо равно 12")
     array
   }

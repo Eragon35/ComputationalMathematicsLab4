5c55917f8432b7add29c081140201dfcd41fa716
Eragon35 <antipin35@yahoo.com>
2021-04-26 20:29:43 +0300

    Fixed power approximation

--- src/main/scala/prog/ApproximationMethods/Power.scala
+++ src/main/scala/prog/ApproximationMethods/Power.scala
@@ -1,22 +1,28 @@
 package prog.ApproximationMethods
 
+import prog.ApproximationMethods.MatrixStaff.Cramer
+
 import scala.collection.mutable
 
 object Power {
   def solve(map: mutable.SortedMap[Double, Double]): Approximation = {
-    var SX: Double = 0.0
-    var SXX: Double = 0.0
-    var SY: Double = 0.0
-    var SXY: Double = 0.0
+    var LnX: Double = 0.0
+    var Ln2X: Double = 0.0
+    var LnY: Double = 0.0
+    var LnYLnX: Double = 0.0
     for ((x, y) <- map) {
-      SX += Math.log(x)
-      SXX += Math.pow(Math.log(x), 2)
-      SY += Math.log(y)
-      SXY += x * Math.log(y)
+      LnX += Math.log(x)
+      Ln2X += Math.pow(Math.log(x), 2)
+      LnY += Math.log(y)
+      LnYLnX += Math.log(x) * Math.log(y)
     }
-    val delta = SXX * map.size - SX * SX
-    val delta1 = SXX * SY - SX * SXY
-    val delta2 = SXY * map.size - SX * SY
+    val delta = Ln2X * map.size - LnX * LnX
+    val delta1 = Ln2X * LnY - LnX * LnYLnX
+    val delta2 = LnYLnX * map.size - LnX * LnY
+//val answer = Cramer.findSolution(LnX, Ln2X, LnY, LnYLnX, map.size)
+//    val a = Math.pow(Math.E, answer._1)
+//    val b = answer._2
+
     val a = Math.pow(Math.E, delta1 / delta)
     val b = delta2 / delta
 
